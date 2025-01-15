4b8808e6ad7e561238120adeb05ba0559f5fe995
SBerger_DEV <teebeutel.de@gmail.com>
2025-01-15 19:40:42 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:gallery_app/celestial.dart';
+import 'list_of_celestials.dart';
+
+void main() {
+  runApp(const MainApp());
+
+  // celestials[0].nameofCelestial
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+}
+
+
+geheimnisskrämerisch

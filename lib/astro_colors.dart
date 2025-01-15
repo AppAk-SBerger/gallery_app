f4f4fd658cdef70d936bdec8c86b8ac4259c6e24
SBerger_DEV <teebeutel.de@gmail.com>
2025-01-15 19:59:23 +0100

    add custom colors abstract class AstroColors

---
+++ lib/astro_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AstroColors {
+  static const Color astroBlue1 = Color(0xFF264eb5);
+  static const Color astroBlue2 = Color(0xFF0f1a61);
+  static const Color astroBlue3 = Color(0xFF151231);
+  static const Color astroBlue4 = Color(0xFF546eb0);
+  static const Color astroGrey1 = Color(0xFF4d4560);
+  static const Color astroGrey2 = Color(0xFF342f41);
+  static const Color astroWhite1 = Color(0xFFe5ecf4);
+  static const Color astroOrange1 = Color(0xFFdd6b09);
+  static const Color astroRed1 = Color(0xFFe3170a);
+}
